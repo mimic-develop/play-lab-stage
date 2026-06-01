e746a05d71bd73fefa2edccd63270c3d52297662
heaminjo <157016835+heaminjo@users.noreply.github.com>
2026-06-01 15:48:11 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/play-lab-stage/sw.js', { scope: '/play-lab-stage/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
