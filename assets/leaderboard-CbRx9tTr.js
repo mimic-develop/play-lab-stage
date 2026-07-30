0ba5b10c5b4ce325c0040a75c570aaaed7bce803
heaminjo <157016835+heaminjo@users.noreply.github.com>
2026-07-30 18:51:45 +0900

    Updates

--- assets/leaderboard-CbRx9tTr.js
+++
@@ -1,2 +0,0 @@
-import{a as n}from"./index-DT4MWJsl.js";function r(a){return{persona:a,entries:[],me:null,myProgress:null,minQualifyHands:50,window:200}}async function l(a){const e=a?`?persona=${a}`:"";return n(`/play-lab/heads-up/leaderboard${e}`).catch(()=>r(a??null))}export{l as g};
-//# sourceMappingURL=leaderboard-CbRx9tTr.js.map
